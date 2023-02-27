5143b38234b0d35253a2246fa55ba946bce1e9b0
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2023-02-27 22:56:44 +0100

    pushed patrones

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+P
+ Original.src/ChessGameBoard.java,6/a/6a3dd969047ca1d30119c8bbf03dc1b76cea19fa
+K
+Original.src/ChessMain.java,9/9/991529c914e1dfb2ed6ec1e5364b228e98969008
---
+++ Original.src/BishopFactory.java
@@ -0,0 +1,6 @@
+public class BishopFactory implements ChessGamePieceFactory {
+    @Override
+    public ChessGamePiece createPiece(ChessGameBoard board, int row, int col, int color) {
+        return new Bishop(board, row, col, color);
+    }
+}
