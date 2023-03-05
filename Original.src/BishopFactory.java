2d6f943f6bee6423de661c62da986b30532d445c
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2023-03-05 23:34:23 +0100

    push test

--- Original.src/BishopFactory.java
+++ Original.src/BishopFactory.java
@@ -1,4 +1,5 @@
 public class BishopFactory implements ChessGamePieceFactory {
+    //
     @Override
     public ChessGamePiece createPiece(ChessGameBoard board, int row, int col, int color) {
         return new Bishop(board, row, col, color);
